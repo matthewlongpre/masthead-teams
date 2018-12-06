4e24c5c2a9977c93e3dab5087385d0d1ce9c8415
Matt Longpre <MacPre@Matts-MacBook-Pro-2.local>
2018-12-06 07:36:23 -0800

    Basic implementation

--- src/App.js
+++ src/App.js
@@ -7,19 +7,101 @@ microsoftTeams.initialize();
 
 class MastheadContainer extends Component {
   state = {
-    data: data
+    data: data,
+    // menuState: []
+    // flatMenu: new Map(),
+    // menuState: []
   }
+
+  componentDidMount() {
+    this._processNav(data.menu);
+  }
+
+  _processNav(menu) {
+    const flatMenu = new Map();
+    menu.forEach(menu => this._flattenMenu(menu, flatMenu));
+    this.setState({
+      flatMenu: flatMenu
+    });
+  }
+
+  _flattenMenu(item, container) {
+    container.set(item._id, item);
+    if (item.menu && item.menu.length !== 0) {
+      item.menu.forEach(menu => {
+        this._flattenMenu(menu, container);
+      });
+    }
+  }
+
+  _changeMenuState(index, id) {
+    let output = [... this.state.menuState].slice(0, index + 1);
+    if (output[index] === id) {
+      output = output.slice(0, index);
+    } else {
+      output[index] = id;
+    }
+    this.setState({
+      menuState: output
+    });
+  }
+
+  // _changeMenuState(index, id) {
+  //   this.setState({
+  //     menuState: [index, id]
+  //   });
+  // }
+
   render() {
-    const { data } = this.state;
-    const items = data.menu.map(item => <Tile key={item._id} {...item} />);
+    const { data, menuState } = this.state;
+
+    let menu = data.menu;
+
     return (
       <div className="masthead-container">
         <header className="masthead-header">
           <img className="logo" src={logo} />
         </header>
-        <div className="masthead-items">
-          {items}
-        </div>
+        <MastheadList depth={0} items={menu} _changeMenuState={(index, id) => this._changeMenuState(index, id)} />
+      </div>
+    );
+  }
+}
+
+class MastheadList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subitems: []
+    };
+  }
+
+  // componentWillReceiveProps(nextProps) {
+  //   for (let i = 0; i < nextProps.items.length; i++) {
+  //     if (nextProps.items[i]._id === nextProps.menuState[nextProps.depth]) {
+  //       this.setState({
+  //         subItems: nextProps.items[i].menu
+  //       });
+  //       return;
+  //     }
+  //   }
+  //   this.setState({
+  //     subItems: []
+  //   });
+  // }
+
+  _handleClick(index, id, event) {
+    console.log(index, id, event);
+    event.nativeEvent.stopImmediatePropagation();
+    this.props._changeMenuState(index, id);
+  }
+
+  render() {
+    const items = this.props.items.map((item, index) => <Tile index={index} key={item._id} {...item} _handleClick={(index, id, event) => this._handleClick(index, id, event)} />);
+
+    return (
+      <div className="masthead-items">
+        {items}
       </div>
     );
   }
@@ -28,7 +110,7 @@ class MastheadContainer extends Component {
 const Tile = (props) => {
   return props.menu && props.menu.length !== 0
     ?
-    <button className="masthead-tile">
+    <button className="masthead-tile" onClick={(event) => props._handleClick(props.depth, props._id, event)}>
       <MastheadItem {...props} />
     </button>
     :
@@ -45,7 +127,5 @@ const MastheadItem = (props) => {
     </Fragment>
   );
 }
-
-
 
 export default MastheadContainer;
