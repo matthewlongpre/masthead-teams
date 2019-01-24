94076bb653ce4105379bcceb9a40912fbd9f4aff
Matt Longpre <mlongpre@itgroove.ca>
2019-01-24 09:07:40 -0800

    Refactored how the Teams script is imported

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MastheadContainer from './components/masthead-container.component';
 import * as serviceWorker from './serviceWorker';
+import './microsoft-teams-app.js';
 
 ReactDOM.render(<MastheadContainer />, document.getElementById('root'));
 
---
+++ src/microsoft-teams-app.js
@@ -0,0 +1,23 @@
+import * as microsoftTeams from "@microsoft/teams-js";
+
+// Call the initialize API first
+microsoftTeams.initialize();
+
+// Save configuration changes
+microsoftTeams.settings.registerOnSaveHandler(function (saveEvent) {
+  // Let the Microsoft Teams platform know what you want to load based on
+  // what the user configured on this page
+  microsoftTeams.settings.setSettings({
+    contentUrl: 'https://itgux.azurewebsites.net/masthead-teams-app/', // Mandatory parameter
+    entityId: 'https://itgux.azurewebsites.net/masthead-teams-app/' // Mandatory parameter
+  });
+
+  // Tells Microsoft Teams platform that we are done saving our settings. Microsoft Teams waits
+  // for the app to call this API before it dismisses the dialog. If the wait times out, you will
+  // see an error indicating that the configuration settings could not be saved.
+  saveEvent.notifySuccess();
+});
+
+// This API tells Microsoft Teams to enable the 'Save' button. Since Microsoft Teams always assumes
+// an initial invalid state, without this call the 'Save' button will never be enabled.
+microsoftTeams.settings.setValidityState(true);
